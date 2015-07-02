775b39ec7de4fc731980b11e38d63d3be24d06a4
Eevee (Alex Munroe) <eevee.git@veekun.com>
2015-07-01 20:17:47 -0700

    Initial commit, with forflor layout!

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+setup(
+    name='forbiddenflora_com',
+    version='0.0',
+    description='forbiddenflora.com',
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
